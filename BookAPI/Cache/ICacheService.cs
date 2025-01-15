30bdf66e0b952995d3f870753357f911abde6c25
acid <vitiazagortsev@gmail.com>
2025-01-15 19:48:33 +0300

    test commit number 6

--- BookAPI/Cache/ICacheService.cs
+++ BookAPI/Cache/ICacheService.cs
@@ -11,7 +11,6 @@
         /// <param name="key">Ключ для вставки в кэш</param>
         /// <param name="value">Значние, доступное по ключу key</param>
         /// <param name="expirationTime">Время истечения хранения ключа (когда он должен удалиться из кэша)</param>
-        /// <returns></returns>
         public Task<bool> SetDataAsync<T>(string key, T value, DateTimeOffset expirationTime);
 
         public Task<bool> RemoveDataAsync(string key);
